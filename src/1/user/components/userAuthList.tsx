36aab22bc4e5b1ee1c6b8a3dd81f75717591b7aa
Gaydnn <haydnhinks@outlook.com>
2023-07-13 11:42:41 +1000

    starting with the table

--- src/1/user/components/userAuthList.tsx
+++ src/1/user/components/userAuthList.tsx
@@ -1,8 +1,40 @@
 import { type NextPage } from "next";
-import { SkeletonCard } from "../../gen/components/skeleton";
+import { SkeletonListRow } from "../../gen/components/skeleton";
 import { IeCard } from "@/components/ie/ie-card";
-import { api } from "@/src/utils/api";
+import { type RouterOutputs, api } from "@/src/utils/api";
 import DebugView from "@/src/1/test/debug-view";
+import {type  ColumnDef } from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+type inferredOutputList = RouterOutputs["userBasic"]["userList"];
+type inferredOutputItem = inferredOutputList["list"][0]
+
+
+export const columns: ColumnDef<inferredOutputItem>[] = [
+  {
+    accessorKey: "name_first",
+    header: "Name"
+  },
+  {
+    accessorKey: "email",
+    header: "Email"
+  },
+  {
+    accessorKey: "business",
+    header: "Business Name"
+  },
+
+]
+
+
 
 export const UserAuthList: NextPage = () => {
   const userList = api.userBasic.userList.useQuery(
@@ -12,9 +44,30 @@ export const UserAuthList: NextPage = () => {
     }
   );
 
+
   return (
     <>
       <div>
+        <div className="w-full border-2 border-pink-300">
+          {/* table section */}
+          <Table>
+            <TableCaption>
+              Table of something
+            </TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Soemthing1</TableHead>
+                <TableHead>Somethign 2</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableCell>asdhiasido</TableCell>
+              <TableCell>asdjiiaojsd</TableCell>
+              <TableCell>sadihaiosd</TableCell>
+              <TableCell>asdaisdj</TableCell>
+            </TableBody>
+          </Table>
+        </div>
         <p>Testing</p>
         <DebugView
           visible
